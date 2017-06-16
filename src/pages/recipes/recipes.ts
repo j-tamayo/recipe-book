dffb31a223e78e2dbec0f2c7c63ba1bfd50dee6e
junior.pena <jtamayodev@gmail.com>
2017-06-16 17:27:53 -0400

    Listing the recipes

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { EditRecipePage } from '../edit-recipe/edit-recipe';
+
+import { RecipesService } from '../../services/recipes';
+import { Recipe } from '../../models/recipe';
 
 @IonicPage()
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html',
 })
-export class RecipesPage {
+export class RecipesPage implements OnInit {
+  recipes : Recipe[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private recipesService: RecipesService
+  ) {
+  }
+
+  ngOnInit() {
+    this.loadItems();
+  }
+
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
+  onLoadRecipe() {
+
+  }
+
+  loadItems() {
+    this.recipes = this.recipesService.getRecipes();
   }
 
   onNewRecipe() {
