2ff1922282667e811fca94736237bc3ddaa78c04
junior.pena <jtamayodev@gmail.com>
2017-06-19 15:40:12 -0400

    Implementing sginin

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../../services/auth';
 
 @IonicPage()
 @Component({
@@ -9,11 +11,37 @@ import { NgForm } from '@angular/forms';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {
+
   }
 
   onSignin(form: NgForm) {
-    console.log(form.value);
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you in...'
+    });
+    loading.present();
+
+    this.authService.signin(form.value.email, form.value.password)
+      .then(data => {
+        loading.dismiss();
+      })
+      .catch(error => {
+        loading.dismiss();
+        
+        const alert = this.alertCtrl.create({
+          title: 'Signin failed!',
+          message: error.message,
+          buttons: ['Ok']
+        });
+
+        alert.present();
+      });
   }
 
 }
